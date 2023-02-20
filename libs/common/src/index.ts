8b28f1ec04741fcc5b22bc0fc8bd6300f9346b47
Elbouchouki Ahmed <elbouchoukigamer@gmail.com>
2023-02-20 09:48:53 +0100

    (prisma & dockerfiles & docker-compose) configurations

---
+++ libs/common/src/index.ts
@@ -0,0 +1,8 @@
+export * from './database/prisma.module';
+export * from './database/prisma.provider';
+export * from './database/prisma.repository';
+
+export * from './rmq/rmq.module';
+export * from './rmq/rmq.service';
+
+export * from './types/graphql';
